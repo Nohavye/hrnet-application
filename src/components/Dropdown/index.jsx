cd7dcbab3f9c8f6976d00debbffeec76030d45e7
Nohavye <noha.poncelet@gmail.com>
2023-10-30 15:10:53 +0100

    Style Dropdown component.

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,17 +1,23 @@
-// Styles
-import styles from './styles.module.css'
-
 // Components
 import { DropdownSelector } from '@hrnet-org/hrnet-component-library'
+
+// Styles
+import styles from './styles.module.css'
 
 export default function Component({ id, label, items, placeholder }) {
     return (
         <div className={styles.wrapper}>
             {label && <label htmlFor={id}>{label}</label>}
             <DropdownSelector
-                classNames={{ wrapper: styles.dropdownWrapper }}
+                classNames={{
+                    wrapper: styles.dropdownWrapper,
+                    input: styles.dropdownInput,
+                    items_wrapper: styles.dropdownItemsWrapper,
+                    item: styles.dropdownItem,
+                }}
                 id={id}
                 items={items}
+                maxHeight={400}
                 placeholder={placeholder}
             />
         </div>
